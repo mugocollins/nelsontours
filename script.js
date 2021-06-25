30723f4a3ea150f03dd7d3470d8937d81f4454da
mugocollins <86485861+mugocollins@users.noreply.github.com>
2021-06-25 19:06:17 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " NelsonTours. All rights reserved.</p>";
+"<p>&copy;  " + new Date().getFullYear() + " <a href='contact.html'>Contact Us</a></p>";
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='tours.html'>Tours</a></li>" +
+"<li><a href='contact.html'>Contact</a></li>" +
+"<li><a href='about.html'>About</a></li>" +
+"</ul>"; 
